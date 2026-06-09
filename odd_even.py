54299a07950b9511ed19bb814d2ef00661fbabd0
HasanBabuShaik <hasanbabushaik15@gmail.com>
2026-06-09 14:01:06 +0530

    Add files via upload
    python program to find the number is odd or even using conditional statements

---
+++ odd_even.py
@@ -0,0 +1,12 @@
+#Check whether a number is even or odd.
+number=int(input("enter a number:"))
+if number%2==0:
+    print("number is even")
+else:
+    print("number is odd")   
+        
+ #number is variable that holds the integer type data   
+ #input()is used to get the input from the user through keyboard
+ #defalutly input() function takes the data as String                           
+#int()function is used to convert the String into integer this is called explicit type conversion
+#print() is used to display the output in the console
