77dbf903cfc8adb297a3960f665540ee5d4b43ea
HasanBabuShaik <hasanbabushaik15@gmail.com>
2026-06-09 15:20:38 +0530

    Add files via upload
    simple calculator program using conditional statements

---
+++ Simplecalculator.py
@@ -0,0 +1,20 @@
+a=int(input(" enter a value :"))
+b=int(input(" enter b value :"))
+operator=input(" enter the operator:")
+if operator=='+':
+        print(a+b)
+elif operator =='-':
+      print(a-b)
+elif operator == '/':
+      print(a/b)
+elif operator == '*':
+      print(a*b)
+elif operator == '**':
+      print(a**b)
+elif operator == '%':
+      print(a%b)
+elif operator =='//':
+      print(a//b)
+else:
+    print("Invalid operator! enter a valid operator")
+     
